f848b296f0a829596a090c09e8df258441eec997
is-hoku <me@hokuishi.be>
2022-09-27 23:45:46 +0900

    remove scripts

--- webapi/script/startup.sh
+++ webapi/script/startup.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-chmod 777 ./script/migrate-up
-chmod 777 ./script/migrate-down
-chmod 777 ./script/migrate-create
-chmod 777 ./script/sql-generate
-chmod 777 ./script/plantuml
 air -c .air.toml
